5b1de02e883de8a0c412281c7126ecd8b916d1b0
spapbp <spapbp045@gmail.com>
2021-03-02 14:09:24 -0500

    Update webScaper.py

--- webScaper.py
+++ webScaper.py
@@ -18,50 +18,17 @@ page_soup = soup(page_html, "html.parser")
 cells = page_soup.findAll("div", {"class":"item-cell"})
 
 #put each cell in cell and grab title
-
-'''
-for cell in cells:
-    title = cell.findAll("a", {"class":"item-title"})
-    print(title[0].text)
-
-    if cell.div.div.p.text == "OUT OF STOCK":
-        #print(cell.div.div.p.text)
-        price = cell.findAll("li", {"class": "price-current"})
-        name = title[0].text
-        
-        
-    
-    else:
-        price = cell.findAll("li", {"class": "price-current"})
-        print(price[0].strong.text, price[0].sup.text)
-'''
-
-
-
 for cell in cells:
     title = cell.findAll("a", {"class":"item-title"})
     price = cell.findAll("li", {"class": "price-current"})
     stock = cell.find("p", {"class": "item-promo"})
 
+    #if there is a p tad with class item-promo then check if it is equal to "OUT OF STOCK" else email name and price
     if stock != None:
         if cell.div.div.p.text == "OUT OF STOCK":
             pass
-            '''
-            name = title[0].text
-            print(name)
-
-            nums = price[0].strong.text + price[0].sup.text
-            print(name)
-            concat = name + '\n' + nums
-            print(nums)
-            #alert.email_alert("4042422725@txt.att.net", "IN STOCK", concat)
-            '''
         else:
             name = title[0].text
-            print(name)
-
-            nums = price[0].strong.text + price[0].sup.text
-            print(name)
-            concat = name + '\n' + nums
-            print(nums)
+            price = price[0].strong.text + price[0].sup.text
+            concat = name + '\n' + price
             #alert.email_alert("Your_Email", "IN STOCK", concat)
